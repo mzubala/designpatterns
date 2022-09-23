ac21f0fb5b5c832f4bfb88c7e4c5955c42cfd498
Maciej Zubala <maciej.zubala@gmail.com>
2022-09-23 23:41:04 +0200

    prep

--- src/main/java/pl/com/bottega/designpatterns/ecom/Money.java
+++ src/main/java/pl/com/bottega/designpatterns/ecom/Money.java
@@ -1,11 +1,22 @@
 package pl.com.bottega.designpatterns.ecom;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Currency;
+import java.util.Locale;
 
 record Money(BigDecimal value, Currency currency) implements Comparable<Money> {
 
-    Money add() {
+    Money(BigDecimal value) {
+        this(value, Currency.getInstance(Locale.getDefault()));
+    }
+
+    Money(BigDecimal value, Currency currency) {
+        this.value = value.setScale(2, RoundingMode.HALF_UP);
+        this.currency = currency;
+    }
+
+    Money add(Money other) {
         return null;
     }
 
