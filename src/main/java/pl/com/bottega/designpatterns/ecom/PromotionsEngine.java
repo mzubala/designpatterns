a39f15fbeafb58da9e9b219f8f519a9e8ca5b387
Maciej Zubala <maciej.zubala@gmail.com>
2022-09-28 10:05:37 +0200

    solution

--- src/main/java/pl/com/bottega/designpatterns/ecom/PromotionsEngine.java
+++ src/main/java/pl/com/bottega/designpatterns/ecom/PromotionsEngine.java
@@ -2,7 +2,17 @@ package pl.com.bottega.designpatterns.ecom;
 
 import io.vavr.control.Option;
 
+import java.math.BigDecimal;
+import java.nio.Buffer;
+import java.time.Clock;
+import java.time.temporal.ChronoField;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 class PromotionsEngine implements RebatePolicy {
 
@@ -14,8 +24,9 @@ class PromotionsEngine implements RebatePolicy {
 
     @Override
     public List<Rebate> calculate(Cart cart) {
-        // TODO implement calculate method
-        return null;
+        List<Rebate> rebates = new LinkedList<>();
+        firstPromotion.calculate(cart, rebates);
+        return rebates;
     }
 }
 
@@ -42,15 +53,47 @@ class BeautyPromotion extends Promotion {
 
     @Override
     void calculate(Cart cart, List<Rebate> accumulator) {
-        // TODO do work and call proceed(...)
+        var beautyProducts = cart.getItems().stream().map(item -> item.product()).filter(item -> item.hasCategory("Uroda"));
+        var rebates = beautyProducts.map(product -> new Rebate(product.id(), "-10% dla urody", product.price().times(new BigDecimal(0.1))));
+        accumulator.addAll(rebates.toList());
+        proceed(cart, accumulator);
     }
 }
 
 class XMasPromotion extends Promotion {
 
+    Supplier<Clock> clockSupplier;
+
     @Override
     void calculate(Cart cart, List<Rebate> accumulator) {
-        // TODO do work and call proceed
+        if (xmasTime()) {
+            rebateRate(cart).peek(rate -> {
+                var rebates = cart.getItems().stream().map(item -> rebate(rate, item));
+                accumulator.addAll(rebates.toList());
+            });
+        }
+        proceed(cart, accumulator);
+    }
+
+    private static Rebate rebate(BigDecimal rate, CartItem.Snapshot item) {
+        return new Rebate(item.product().id(), "XMas Promo", item.product().price().times(rate));
+    }
+
+    private boolean xmasTime() {
+        var now = clockSupplier.get().instant();
+        var month = now.get(ChronoField.MONTH_OF_YEAR);
+        var day = now.get(ChronoField.DAY_OF_MONTH);
+        return month == 12 && day >= 20 && day <= 24;
+    }
+
+    private Option<BigDecimal> rebateRate(Cart cart) {
+        if (cart.getCustomer().customerType() == CustomerType.CHAMPION) {
+            return Option.of(new BigDecimal("0.5"));
+        } else if (cart.getCustomer().customerType() == CustomerType.FREQUENT) {
+            return Option.of(new BigDecimal("0.3"));
+        } else {
+            return Option.none();
+        }
     }
 }
 
@@ -58,7 +101,14 @@ class FoodPromotion extends Promotion {
 
     @Override
     void calculate(Cart cart, List<Rebate> accumulator) {
-        // TODO do work and call proceed
+        var foodItems = cart.getItems().stream().filter(item -> item.product().hasCategory("Uroda"));
+        var itemsCount = foodItems.collect(Collectors.summarizingInt(CartItem.Snapshot::count));
+        if (itemsCount.getSum() >= 5) {
+            var sortedProducts = foodItems.map(CartItem.Snapshot::product)
+                .sorted(Comparator.comparing(Product::price));
+            var cheapestProduct = sortedProducts.findFirst();
+            cheapestProduct.ifPresent(product -> accumulator.add(new Rebate(product.id(), "-5% na jedzenie", product.price().times(new BigDecimal(0.05)))));
+        }
     }
 }
 
